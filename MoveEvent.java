8f7f4b0846c2a4252a59225c8e2f011a53e63eb0
Jerry H. Wang <jwang35@vm-hw04.eecs.tufts.edu>
2021-12-14 10:26:31 -0500

    sixth commit make sure it compiles

--- MoveEvent.java
+++ MoveEvent.java
@@ -43,7 +43,7 @@ public class MoveEvent implements Event {
       mbta.train_position.replace(t.toString(), null);
 
       while (startIndex != endIndex) {
-        if ((startIndex == maxIndex && t.isRight()) || (startIndex == 0 && !t.isRight()) {
+        if ((startIndex == maxIndex && t.isRight()) || (startIndex == 0 && !t.isRight())) {
           t.changeDir();
         }
 
