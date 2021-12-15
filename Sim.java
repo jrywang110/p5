94707cb8c850cd2371b4c9ca47df04e15a4e193b
Jerry H. Wang <jwang35@vm-hw04.eecs.tufts.edu>
2021-12-14 20:40:42 -0500

    37 huge commit right here

--- Sim.java
+++ Sim.java
@@ -25,7 +25,6 @@ public class Sim {
               synchronized (this) {
                 Lock currLock = stationLocks.get(currStation);
                 currLock.lock();
-                sleep(500);
 
                 if (((stationList.indexOf(currStation) == stationList.size() - 1) && t.isRight()) || (stationList.indexOf(currStation) == 0 && !t.isRight())) {
                       t.changeDir();
